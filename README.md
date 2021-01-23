9ccd5ec2b356156823ca2dd0f5e0129315d01c14
Piotr Synowiec <psynowiec@gmail.com>
2021-01-23 11:31:42 +0100

    #dev: refactor table create and delete, new command calculate-fold

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 # Seismic fold calculation - cli scripts
 
+## Install
+* clone repository `git clone https://github.com/mysiar/seismic-fold-calculation-cli.git`
+* `cd seismic-fold-calculation-cli`
+* `sh install.sh`
+
 ## Project file definition
 ```json
 {
   "db_url": "postgresql://user:password@db_host/db_fold",
-  "grid_file": "grid.json"
+  "grid_file": "grid.json",
+  "sps_file": "your SPS source file",
+  "rps_file": "your SPS receiver file",
+  "xps_file": "your SPS relation file",
+  "verbose": 1
 }
 ```
+_verbose_ - to display information during different operations
+
 
 ## Commands
 * `sfc-create-db-table` - creates *bins* table in database
 * `sfc-delete-db-table` - deletes *bins* table in database
+* `sfc-calculate-fold` - calculates fold and writes it to CSV file
 
+## How to use it
 
 
 
--- bin/sfc_app.py
+++ bin/sfc_app.py
@@ -1,17 +1,29 @@
 import json
-
-from SeismicFoldDbGis.FoldDbGis import FoldDbGis
 from sqlalchemy import create_engine
 
+from SeismicFoldDbGis.FoldDbGis import FoldDbGis
+from FixedWidthTextParser.Seismic.SpsParser import Sps21Parser
+from SeismicFold.Grid import Grid
+from SeismicFold.Fold import Fold
 
-def create_db_table(db_url: str):
-    engine = create_engine(db_url, echo=True)
+DB_URL = 'db_url'
+GRID = 'grid_file'
+SPS = 'sps_file'
+RPS = 'rps_file'
+XPS = 'xps_file'
+VERBOSE = 'verbose'
+
+
+def create_db_table(prj_file: str):
+    prj = read_project_file(prj_file)
+    engine = create_engine(prj[DB_URL], echo=bool(prj[VERBOSE]))
     fold = FoldDbGis(db_engine=engine)
     fold.create_table()
 
 
-def delete_db_table(db_url: str):
-    engine = create_engine(db_url, echo=True)
+def delete_db_table(prj_file: str):
+    prj = read_project_file(prj_file)
+    engine = create_engine(prj[DB_URL], echo=bool(prj[VERBOSE]))
     fold = FoldDbGis(db_engine=engine)
     fold.delete_table()
 
@@ -25,3 +37,22 @@ def read_project_file(filename: str):
     result = json.load(file)
     file.close()
     return result
+
+
+def calculate_fold(prj_file: str, fold_csv_file: str):
+    prj = read_project_file(prj_file)
+    parser = Sps21Parser()
+    grid = Grid()
+    grid.read(prj['grid_file'])
+    fold = Fold(
+        grid=grid,
+        parser=parser,
+        sps_file=prj['sps_file'],
+        rps_file=prj['rps_file'],
+        xps_file=prj['xps_file'],
+        verbose=bool(prj['verbose'])
+    )
+
+    fold.load_data()
+    fold.calculate_fold()
+    fold.write_fold2csv(fold_csv_file)
