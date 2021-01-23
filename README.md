ad2543e735c41ba2b7a1ad08432909f3bab73f69
Piotr Synowiec <psynowiec@gmail.com>
2021-01-23 12:28:15 +0100

    #dev: fold load & update

--- README.md
+++ README.md
@@ -13,18 +13,36 @@
   "sps_file": "your SPS source file",
   "rps_file": "your SPS receiver file",
   "xps_file": "your SPS relation file",
-  "verbose": 1
+  "verbose": 1,
+  "db_verbose": 1,
 }
 ```
-_verbose_ - to display information during different operations
-
+* _verbose_ - to display information during different operations
+* _db_verbose_ - SQLAlchemy echo
 
 ## Commands
 * `sfc-create-db-table` - creates *bins* table in database
 * `sfc-delete-db-table` - deletes *bins* table in database
 * `sfc-calculate-fold` - calculates fold and writes it to CSV file
+* `sfc-db-fold-load` - load fold from CSV file to db
+* `sfc-db-fold-update` - update fold from CSV file to db
 
 ## How to use it
 
+### Scenario 1 - single SPS data set
+* create Spatial DB (PostgreSQL or SQLite)
+* create bins table `sfc-create-db-table` 
+* create project
+* calculate fold `sfc-calculate-fold`
+* load fold to db `sfc-db-fold-load`
+
+### Scenario 2 - multiple SPS data set
+* TODO
+
+
+## More info
+
+* [Seismic Fold](https://github.com/mysiar/seismic-fold-python-package)
+* [Seismic Fold Db GIS](https://github.com/mysiar/seismic-fold_db_gis-python-package)
 
 
--- bin/sfc_app.py
+++ bin/sfc_app.py
@@ -12,18 +12,19 @@ SPS = 'sps_file'
 RPS = 'rps_file'
 XPS = 'xps_file'
 VERBOSE = 'verbose'
+DB_VERBOSE = 'db_verbose'
 
 
-def create_db_table(prj_file: str):
+def db_table_create(prj_file: str):
     prj = read_project_file(prj_file)
-    engine = create_engine(prj[DB_URL], echo=bool(prj[VERBOSE]))
+    engine = _create_db_engine(db_url=prj[DB_URL], db_verbose=bool(prj[DB_VERBOSE]))
     fold = FoldDbGis(db_engine=engine)
     fold.create_table()
 
 
-def delete_db_table(prj_file: str):
+def db_table_delete(prj_file: str):
     prj = read_project_file(prj_file)
-    engine = create_engine(prj[DB_URL], echo=bool(prj[VERBOSE]))
+    engine = _create_db_engine(db_url=prj[DB_URL], db_verbose=bool(prj[DB_VERBOSE]))
     fold = FoldDbGis(db_engine=engine)
     fold.delete_table()
 
@@ -39,7 +40,7 @@ def read_project_file(filename: str):
     return result
 
 
-def calculate_fold(prj_file: str, fold_csv_file: str):
+def fold_calculate(prj_file: str, fold_csv_file: str):
     prj = read_project_file(prj_file)
     parser = Sps21Parser()
     grid = Grid()
@@ -56,3 +57,23 @@ def calculate_fold(prj_file: str, fold_csv_file: str):
     fold.load_data()
     fold.calculate_fold()
     fold.write_fold2csv(fold_csv_file)
+
+
+def fold_db_load(prj_file: str, fold_csv_file: str):
+    prj = read_project_file(prj_file)
+    engine = _create_db_engine(db_url=prj[DB_URL], db_verbose=bool(prj[DB_VERBOSE]))
+    fold = FoldDbGis(db_engine=engine, verbose=prj[VERBOSE])
+    fold.load_from_csv(fold_csv_file)
+
+
+def fold_db_update(prj_file: str, fold_csv_file: str):
+    prj = read_project_file(prj_file)
+    engine = _create_db_engine(db_url=prj[DB_URL], db_verbose=bool(prj[DB_VERBOSE]))
+    fold = FoldDbGis(db_engine=engine, verbose=prj[VERBOSE])
+    fold.update_from_csv(fold_csv_file)
+
+
+def _create_db_engine(db_url: str, db_verbose: bool):
+    # TODO : sqlite
+    engine = create_engine(db_url, echo=db_verbose)
+    return engine
